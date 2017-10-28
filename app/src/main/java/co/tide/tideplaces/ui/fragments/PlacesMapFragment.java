e2ac00f7e5e7caebfcdaa1e91ab30d3db66e5deb
Antonis Latas <latas.ceid@gmail.com>
2017-10-28 19:20:43 +0300

    - add google play services - add map in MapFragment

--- app/src/main/java/co/tide/tideplaces/ui/fragments/PlacesMapFragment.java
+++ app/src/main/java/co/tide/tideplaces/ui/fragments/PlacesMapFragment.java
@@ -2,17 +2,43 @@ package co.tide.tideplaces.ui.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import co.tide.tideplaces.R;
 
-public class PlacesMapFragment extends Fragment {
+public class PlacesMapFragment extends Fragment implements OnMapReadyCallback {
+
+    @BindView(R.id.mapView)
+    MapView mapView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.map_fragment_layout, container, false);
+        ButterKnife.bind(this, view);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
         return view;
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.i("MAP","MAP IS READY");
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
     }
 }

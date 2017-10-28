ce142074a317bd028fcf7b7b6b1c4d7e20bac221
Antonis Latas <latas.ceid@gmail.com>
2017-10-28 16:21:02 +0300

    Initial commit

---
+++ app/src/main/java/co/tide/tideplaces/PlacesActivity.java
@@ -0,0 +1,17 @@
+package co.tide.tideplaces;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PlacesActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_places);
+
+
+    }
+
+}

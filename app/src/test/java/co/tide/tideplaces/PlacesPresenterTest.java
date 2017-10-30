bb0cdeff10331de5028a6fa0bb3961f7dc902fb4
Antonis Latas <latas.ceid@gmail.com>
2017-10-31 00:20:53 +0200

    Add unit test for PlacesPresenter

--- app/src/test/java/co/tide/tideplaces/PlacesPresenterTest.java
+++ app/src/test/java/co/tide/tideplaces/PlacesPresenterTest.java
@@ -13,10 +13,14 @@ import org.mockito.Mock;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import co.tide.tideplaces.data.interactors.MyLocationRepository;
 import co.tide.tideplaces.data.interactors.PlacesRepository;
 import co.tide.tideplaces.data.models.Place;
+import co.tide.tideplaces.data.models.RxException;
+import co.tide.tideplaces.data.models.error.LocationError;
+import co.tide.tideplaces.data.models.error.UnAuthorizedLocationError;
 import co.tide.tideplaces.data.responses.GSPlaceResult;
 import co.tide.tideplaces.data.responses.GSPlacesResponse;
 import co.tide.tideplaces.data.responses.PlaceResponseGeometry;
@@ -28,6 +32,7 @@ import io.reactivex.Observable;
 import retrofit2.HttpException;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.never;
@@ -35,7 +40,7 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
 
-public class PlacesTest extends BaseTest {
+public class PlacesPresenterTest extends BaseTest {
 
     final int totalPlaces = 20;
 
@@ -53,8 +58,9 @@ public class PlacesTest extends BaseTest {
     PlacesRepository placesRepository;
 
 
-    ConstantParams params = new ConstantParams("apiKey", "type", 5000);
+    ConstantParams params = getRandomParams();
     ArgumentCaptor<List> listCaptor = ArgumentCaptor.forClass(List.class);
+
 
     @Before
     public void setUp() {
@@ -70,11 +76,9 @@ public class PlacesTest extends BaseTest {
         doReturn(Observable.just(new GSPlacesResponse(Collections.<GSPlaceResult>emptyList()))).when(apiService).getPlaces(anyString(), anyString(), anyString(), anyString());
         placesPresenter.showPlaces();
         testScheduler.triggerActions();
-        verify(placesPresenter).onNext(listCaptor.capture());
-        verify(placesScreen).onErrorRetrievingPlaces();
-        Assert.assertEquals(0, listCaptor.getValue().size());
-
-        verify(placesPresenter, never()).onError(any(Throwable.class));
+        verify(placesPresenter, never()).onNext(ArgumentMatchers.<Place>anyList());
+        verify(placesPresenter).onError(any(RxException.class));
+        verify(placesScreen).onErrorRetrievingPlaces(R.string.no_close_places_error);
         verify(placesScreen, never()).showPlaces(ArgumentMatchers.<Place>anyList());
     }
 
@@ -91,7 +95,7 @@ public class PlacesTest extends BaseTest {
         Assert.assertEquals(totalPlaces, listCaptor.getValue().size());
         verify(placesScreen).showPlaces(listCaptor.capture());
         Assert.assertEquals(totalPlaces, listCaptor.getValue().size());
-        verify(placesScreen, never()).onErrorRetrievingPlaces();
+        verify(placesScreen, never()).onErrorRetrievingPlaces(anyInt());
         verify(placesPresenter, never()).onError(any(Throwable.class));
     }
 
@@ -106,9 +110,27 @@ public class PlacesTest extends BaseTest {
         verify(placesPresenter, never()).onNext(ArgumentMatchers.<Place>anyList());
         verify(placesScreen, never()).showPlaces(ArgumentMatchers.<Place>anyList());
         verify(placesPresenter).onError(any(Throwable.class));
-        verify(placesScreen).onErrorRetrievingPlaces();
+        verify(placesScreen).onErrorRetrievingPlaces(R.string.general_error_retrieving_places);
     }
 
+
+    @Test
+    public void presenterTest_NoLocationFound_duo_to_permission() {
+        doReturn(Observable.error(new RxException(new UnAuthorizedLocationError()))).when(myLocationRepository).data();
+        placesPresenter.showPlaces();
+        testScheduler.triggerActions();
+        verify(placesPresenter, never()).onNext(ArgumentMatchers.<Place>anyList());
+        verify(placesScreen).requestLocationPermission();
+    }
+
+    @Test
+    public void presenterTest_NoLocationFound_due_to_system() {
+        doReturn(Observable.error(new RxException(new LocationError()))).when(myLocationRepository).data();
+        placesPresenter.showPlaces();
+        testScheduler.triggerActions();
+        verify(placesPresenter, never()).onNext(ArgumentMatchers.<Place>anyList());
+        verify(placesScreen).onErrorRetrievingPlaces(R.string.location_cannot_retrieved_message);
+    }
 
     private List<GSPlaceResult> getRandomPlaces() {
         List<GSPlaceResult> placeResults = new ArrayList<>();
@@ -116,6 +138,10 @@ public class PlacesTest extends BaseTest {
             placeResults.add(new GSPlaceResult("" + i, "name" + i, new PlaceResponseGeometry(15.0, 15.0)));
         }
         return placeResults;
+    }
+
+    private ConstantParams getRandomParams() {
+        return new ConstantParams("ApikEy", "randomType", new Random().nextInt());
     }
 
 }
